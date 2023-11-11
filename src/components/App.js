34b47ca5f3fb144ea97b6b608be32d66ad1f9af0
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-11 18:36:03 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+const shoppingItems = {
+  male: ["Shirts", "Facial-grooming kits", "Hair styling", "Jeans", "Shoes"],
+  female: ["Tops", "Skirts", "Makeup items", "Jewellery", "Sandals"]
+};
+
 const App = () => {
+  const [items, setItems] = useState(shoppingItems.male)
   return (
     <div>
         {/* Do not remove the main div */}
+        <select name="" id="" onChange={e=>setItems(shoppingItems[e.target.value])}>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+        <ul>
+          {
+            items.map(item=><li>{item}</li>)
+          }
+        </ul>
     </div>
   )
 }
